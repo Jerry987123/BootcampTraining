99ec85b5f6bf0045e94dfcbfe9087c9d992c8cad
JerryWu <plefyion@gmail.com>
2021-06-21 16:11:26 +0800

    加上AFNetworking、JSONModel、SDWebImage library

---
+++ BootcampTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BootcampTraining
+//
+//  Created by ESB21852 on 2021/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewcontroller")
+    }
+
+
+}
+
