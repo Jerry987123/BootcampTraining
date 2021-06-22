401001e39326f2c20b83baa02f2a81c3f6d09a4e
JerryWu <plefyion@gmail.com>
2021-06-22 11:49:00 +0800

    建立Movie Cell

---
+++ BootcampTraining/SearchingView/MovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCell.swift
+//  BootcampTraining
+//
+//  Created by ESB21852 on 2021/6/22.
+//
+
+import UIKit
+
+class MovieCell:UITableViewCell {
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- BootcampTraining/SearchingView/SearchingViewControllerDelegate.swift
+++ BootcampTraining/SearchingView/SearchingViewControllerDelegate.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 extension SearchingViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var result = ""
+        switch section {
+        case 0:
+            result = "電影"
+        case 1:
+            result = "音樂"
+        default:
+            result = "其它"
+        }
+        return result
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = Bundle.main.loadNibNamed("MovieCell", owner: self, options: nil)?.first as! MovieCell
+        return cell
     }
     
     
