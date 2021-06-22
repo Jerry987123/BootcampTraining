5af81b5d4f7e6d5527d4759a194c61c99ce142c9
JerryWu <plefyion@gmail.com>
2021-06-22 10:58:05 +0800

    TabBarViewController樣式設定

--- BootcampTraining/MainTabBar/MainTabBarViewController.swift
+++ BootcampTraining/MainTabBar/MainTabBarViewController.swift
@@ -12,11 +12,30 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         setUI()
     }
-    func setUI(){
+    private func setUI(){
         let searchingView = UIStoryboard(name: "SearchingView", bundle: nil).instantiateViewController(withIdentifier: "SearchingViewController")
         searchingView.tabBarItem.image = UIImage.init(systemName: "magnifyingglass")
         let personalView = UIStoryboard(name: "PersonalView", bundle: nil).instantiateViewController(withIdentifier: "PersonalViewController")
         personalView.tabBarItem.image = UIImage.init(systemName: "person")
         viewControllers = [searchingView, personalView]
+        
+        tabBar.backgroundColor = .gray
+        let singleTabWidth: CGFloat = tabBar.frame.size.width / 2
+        let singleTabSize = CGSize(width:singleTabWidth , height: tabBar.frame.size.height)
+        let selectedTabBackgroundImage = imageWithColor(color: .white, size: singleTabSize)
+        tabBar.selectionIndicatorImage = selectedTabBackgroundImage.resizableImage(withCapInsets: UIEdgeInsets(top: .leastNonzeroMagnitude, left: 0, bottom: 0, right: 0))
+    }
+    private func imageWithColor(color: UIColor, size: CGSize) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image!
     }
 }
